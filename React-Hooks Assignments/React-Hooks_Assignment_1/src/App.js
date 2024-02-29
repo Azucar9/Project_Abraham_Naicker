b275b41fb1aa3c726aa061e5f7dc4e5575832189
Azucar9 <100841285+Azucar9@users.noreply.github.com>
2024-02-29 19:06:11 +0530

    Add files via upload

---
+++ React-Hooks Assignments/React-Hooks_Assignment_1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import VideoCard from "./components/VideoCard";
+
+const App = () => {
+  return (
+    <div>
+      <VideoCard />
+    </div>
+  );
+};
+
+export default App;
